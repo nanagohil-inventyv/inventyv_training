c4ae6ffacbdf0df8d632731881254de1c2654850
nanagohil-inventyv <nana.gohil.inventyv@gmail.com>
2026-01-25 13:26:13 +0530

    javascipt_task 02_task Added

--- 04_JavaScript_Task/02_Task.js
+++ 04_JavaScript_Task/02_Task.js
@@ -1,9 +1,13 @@
-let n = 5
+let n = 5;
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
-        process.stdout.write(
-            ((i < j && i < -(~(n - i)) && i < -(~(n - j)) ? i : (j < -(~(n - i)) && j < -(~(n - j))) ? j :
-            (n - i < n - j ) ? -(~(n - i )) : -(~(n - j ))) + " "));      
+        let di = -(~(n - i));
+        let dj = -(~(n - j));
+        // min4 using ^ and & (no ~ here)
+        let min_ij = j ^ ((i ^ j) & -(i < j));
+        let min_d = dj ^ ((di ^ dj) & -(di < dj));
+        let val = min_d ^ ((min_ij ^ min_d) & -(min_ij < min_d));
+        process.stdout.write(val + " ");
     }
     console.log("");
 }
